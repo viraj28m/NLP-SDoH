c0bb29802852084515b4bb1b9eb7e6a8f314483d
viraj28m <40471377+viraj28m@users.noreply.github.com>
2022-12-07 23:48:07 -0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import transformers
+from transformers import AutoModel, BertTokenizerFast
 
+# specify GPU
+device = torch.device("cuda")
